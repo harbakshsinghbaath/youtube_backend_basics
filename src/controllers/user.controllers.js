adedc21d4a4101731868c6989d3bdbe8e33dca09
harbakshsinghbaath <harbakshsinghbaath@gmail.com>
2025-07-07 16:57:34 +0530

    login logout

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -100,7 +100,36 @@ const loginUser = asyncHandler( async(req,res) => {
     //if verified access and refresh token
     const {accessToken, refreshToken} = await generateAcessAndRefreshToken(user._id);
     //send cookies
+    const loggedInUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    const options = {
+        httpOnly : true,
+        secure : true
+    }
 
+    return res.status(200).cookie("accessToken", accessToken, options).cookie("refreshToken", refreshToken, options).json(
+        new ApiResponse( 200, {user: loggedInUserm accessToken, refreshToken},"User looged in successfully")
+    )
 })
 
-export {registerUser};
+const logoutUser = asyncHandler( async(req,res) => {
+    await User.findByIdAndUpdate(req.user._id,{
+        $set:{
+            refreshToken:undefined
+        }
+    },{
+        new:true
+    })
+
+    const options = {
+        httpOnly: true,
+        secure:true
+    }
+
+    return res.status(200).clearCookie("accessToken", options).clearCookie("refreshToken", options).json(new ApiResponse(200, "User logged out"))
+})
+
+
+
+export {registerUser, loginUser, logoutUser};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from 'express';
-import {registerUser} from "../controllers/user.controllers.js";
+import {loginUser, logoutUser, registerUser} from "../controllers/user.controllers.js";
 import {upload} from "../middleware/multer.middleware.js";
+import {verifyJWT} from "../middleware/auth.middleware.js";
 
 const router = Router();
 router.route('/register').post(upload.fields([
@@ -11,6 +12,10 @@ router.route('/register').post(upload.fields([
         maxCount: 1 }
 ]),
     registerUser);
+
+router.route('/login').post(loginUser)
+
+router.route('/logout').post(verifyJWT, logoutUser)
 
 
 
